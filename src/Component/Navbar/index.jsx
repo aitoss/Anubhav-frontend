6aab046e4aac090b508d0b9f2b4b7086ae939032
arkumish <arpit.cse24@gmail.com>
2021-07-12 23:44:03 +0530

    updated navbar

--- src/Component/Navbar/index.jsx
+++ src/Component/Navbar/index.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Dropdown from 'react-bootstrap/Dropdown'
 import './index.scss'
+import { isMobile } from '../../utils'
+import { dropDownList, mobileDropDownList } from './menuItems'
+
+
+let menuList = (isMobile ? mobileDropDownList : dropDownList)
 
 const Navbar = () => {
     return (
         <>
-        <div className="d-flex justify-content-around  p-2 border-bottom navbar">
-            <Link to='/' className="flex-grow-1 ml-5 bd-highlight title">Anubhav</Link>
+            <div className="d-flex justify-content-around  p-2 border-bottom navbar">
+                <Link to='/' className="flex-grow-1 ml-5 bd-highlight title">Anubhav</Link>
 
-            <div className="action-buttons">
-            
-            <Link to='/write' className="mt-2 btn btn-primary font-weight-bold">Write Article</Link>
-            <Link to='/request' className="mt-2 btn btn-outline-primary">Request Article</Link>
-            <Link to='/guidelines' className="guidelines-logo"> <i className="fa fa-question-circle-o" aria-hidden="true"></i></Link> 
+                <div className="action-buttons">
+
+                    <Link to='/write' className="mt-2 btn btn-primary font-weight-bold">Write Article</Link>
+                    <Link to='/request' className="mt-2 btn btn-outline-primary">Request Article</Link>
+                </div>
+                <Dropdown className="navDropDown">
+                    <Dropdown.Toggle size='lg' variant='white' id="dropdown-basic">
+                        <i class={isMobile ? 'fa fa-bars' : 'fa fa-chevron-circle-down'} aria-hidden="true"></i> </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        {menuList.map((item, index) =>
+                            <Dropdown.Item key={index}>
+                                <Link to={item.link}><i className={item.iconClass} aria-hidden="true"></i>{item.title}</Link>
+                            </Dropdown.Item>
+                        )
+                        }
+                       
+                    </Dropdown.Menu>
+                </Dropdown>
+
+
             </div>
-           
-        </div>
         </>
     )
 }
---
+++ src/Component/Navbar/menuItems.js
@@ -0,0 +1,45 @@
+export const dropDownList = [
+    {
+        link : "/story", 
+        iconClass : "fa fa-history",
+        title : "Our Story"
+    },
+    {
+        link : "/developers", 
+        iconClass : "fa fa-code",
+        title : "Developers"
+    },
+    {
+        link : "/guidelines", 
+        iconClass : "fa fa-info-circle",
+        title : "Guidelines"
+    },
+]
+
+export const mobileDropDownList = [
+    {
+        link : "/write", 
+        iconClass : "fa fa-pencil-square-o",
+        title : "Write Article"
+    },
+    {
+        link : "/request", 
+        iconClass : "fa fa-file-text-o",
+        title : "Request Article"
+    },
+    {
+        link : "/story", 
+        iconClass : "fa fa-history",
+        title : "Our Story"
+    },
+    {
+        link : "/developers", 
+        iconClass : "fa fa-code",
+        title : "Developers"
+    },
+    {
+        link : "/guidelines", 
+        iconClass : "fa fa-info-circle",
+        title : "Guidelines"
+    },
+]
